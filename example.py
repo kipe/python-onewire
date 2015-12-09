6d76b2b2ea0b917324c2ca89153840feba6dfd8e
Kimmo Huoman <kipenroskaposti@gmail.com>
2015-12-09 10:22:29 +0200

    Improve example

--- example.py
+++ example.py
@@ -2,7 +2,16 @@ from onewire import Onewire
 
 # Use USB-adapter
 with Onewire('u') as o:
+
     # Find all temperature sensors in the bus, and print family, id and temperature as float.
-    print(['%s.%s %.02f' % (s.family, s.id, s.read_float('temperature')) for s in o.find(has_all=['temperature'])])
-    # Read temperature from specific sensor (note: when accessing values directly, the return value is always a string).
+    print('\n'.join([
+        '%s.%s %.02f' % (s.family, s.id, s.read_float('temperature'))
+        for s in o.find(has_all=['temperature'])
+    ]))
+
+    # Read temperature from specific sensor
+    # (note: when accessing values directly, the return value is always a string).
     print(o.sensor('28.D035DE060000').temperature)
+
+    # Find all DS18B20 -sensors
+    print([s for s in o.find(sensor_type='DS18B20')])
