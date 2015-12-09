c6ac8693be7e77d87c426063d7eef1cef1f58c51
Kimmo Huoman <kipenroskaposti@gmail.com>
2015-12-09 10:26:43 +0200

    Add version info etc. to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from distutils.extension import Extension
 
 setup(
     name='onewire',
+    version='0.1',
+    description='A wrapper for OWFS C-API, compatible with both Python 2.7 and Python 3.x.',
+    author='Kimmo Huoman',
+    author_email='kipenroskaposti@gmail.com',
+    url='https://github.com/kipe/python-onewire',
     packages=['onewire'],
     ext_modules=[
         Extension('onewire._ow', libraries=['owcapi'], sources=['onewire/_ow.c'], extra_link_args=['-I', '/usr/include'])
